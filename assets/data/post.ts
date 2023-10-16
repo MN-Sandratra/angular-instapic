0b71f66b4a112f5869a89d4c8afe348ccd9854a2
MN-Sandratra <mn.sandratra@gmail.com>
2023-10-16 10:31:10 +0200

    Auto-generated commit [ci skip]

---
+++ assets/data/post.ts
@@ -0,0 +1,43 @@
+export const post = [
+    {
+      "userId": 2,
+      "id": 1,
+      "description": "Une aventure épique commence...",
+      "image": "assets/data/img05.jpg",
+      "like": 453,
+      "createAt": "il y a 3 heures"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "description": "Exploration de nouveaux horizons 🌍",
+      "image": "assets/data/img04.jpg",
+      "like": 1283,
+      "createAt": "il y a 6 heures"
+    },
+    {
+      "userId": 3,
+      "id": 3,
+      "description": "Vue à couper le souffle 😍",
+      "image": "assets/data/img01.jpg",
+      "like": 865,
+      "createAt": "il y a 5 jours"
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "description": "Une journée sous le soleil ☀️",
+      "image": "assets/data/feedPhoto.jpg",
+      "like": 252,
+      "createAt": "il y a 1 heure"
+    },
+    {
+      "userId": 3,
+      "id": 5,
+      "description": "Rencontre avec la nature sauvage 🌿",
+      "image": "assets/data/img03.jpg",
+      "like": 851,
+      "createAt": "il y a 3 heures"
+    }
+  ];
+  
---
+++ assets/data/user.ts
@@ -0,0 +1,38 @@
+export const user=[
+    {
+        "id": 1,
+        "username": "Sandratra",
+        "realName": "MBELO NDRIAMANAMPY Sandratra",
+        "bio":"Étudiant en Master II en informatique à la Faculté des Sciences de Montpellier.",
+        "picture":"assets/webporfolio.png",
+        "follower":205,
+        "following":215,
+    },
+    {
+        "id": 2,
+        "username": "Robert",
+        "realName": "Roberto Carlos",
+        "picture": "assets/profile.jpg",
+        "bio": "🌟 Passionné de photographie et d'aventure. Découvrez le monde à travers mon objectif 📷",
+        "follower": 125,
+        "following": 356
+    },
+    {
+        "id": 3,
+        "username": "Davis23",
+        "realName": "Antony Davis",
+        "picture": "assets/profile2.jpg",
+        "bio": "🏀 Joueur de basket professionnel et amateur de café. La vie est un jeu d'équipe.",
+        "follower": 525,
+        "following": 219
+    },
+    {
+        "id": 4,
+        "username": "KingJames",
+        "realName": "Ervin Howell",
+        "picture": "assets/profile3.jpeg",
+        "bio": "🏀 Le roi du basket-ball. Père. Fier enfant d'Akron. Chaque jour est un slam dunk.",
+        "follower": 925,
+        "following": 215
+    }
+]
